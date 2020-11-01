5ad370b2d4b1be2c4b5165efca7ac282e682c8ea
delryn <hidelryn@gmail.com>
2020-11-01 18:07:30 +0900

    모발겜에서 행동력 구하는 로직 추가

--- action_example/index.js
+++ action_example/index.js
@@ -13,46 +13,49 @@ const connection = mysql.createConnection({
 });
 
 const USER_ID = "1q2w3e";
-const USER_QUERY = "SELECT * FROM user_action WHERE user_id = ?";
-const CHARGE_TIME = 60 * 1000; // ms
+const GET_USER_QUERY = "SELECT * FROM UserActivePoint WHERE user_id = ?";
+const UPDATE_USER_QUERY =
+  "UPDATE UserActivePoint SET last_update_ts = ?, active_point = ? WHERE user_id = ?";
+const CHARGE_TIME = 60 * 1000; // 충전 쿨타임 단위는 ms
+const MAX_ACTIVE_POINT = 5;
 
 app.get("/", (req, res) => {
   const now = Date.now();
-  connection.query(USER_QUERY, [USER_ID], (err, results) => {
+  connection.query(GET_USER_QUERY, [USER_ID], (err, results) => {
     if (err) throw err;
     if (results.length === 0) {
       return connection.query(
-        "INSERT INTO user_action(user_id, last_update_time, action_count) VALUES (?, ?, ?)",
+        "INSERT INTO UserActivePoint(user_id, last_update_ts, active_point) VALUES (?, ?, ?)",
         [USER_ID, 0, 5],
         (err) => {
           if (err) throw err;
           return res.send(
             JSON.stringify({
               code: 200,
-              action_count: 5,
-              last_update_time: 0,
-              server_time: now,
+              active_point: 5,
+              last_update_ts: 0,
+              server_ts: now,
             })
           );
         }
       );
     }
-    const calculate = recoverAction(
-      results[0].action_count,
-      results[0].last_update_time
+    const activePointCalculate = activePointMiddleWare(
+      results[0].active_point,
+      results[0].last_update_ts,
     );
     return connection.query(
-      "UPDATE user_action SET last_update_time = ?, action_count = ? WHERE user_id = ?",
-      [calculate.now, calculate.count, USER_ID],
+      UPDATE_USER_QUERY,
+      [activePointCalculate.last_update_ts, activePointCalculate.active_point, USER_ID],
       (err) => {
         if (err) throw err;
         const now = Date.now();
         return res.send(
           JSON.stringify({
             code: 200,
-            action_count: calculate.count,
-            last_update_time: calculate.now,
-            server_time: now,
+            active_point: activePointCalculate.active_point,
+            last_update_ts: activePointCalculate.last_update_ts,
+            server_ts: now,
           })
         );
       }
@@ -61,26 +64,26 @@ app.get("/", (req, res) => {
 });
 
 app.get("/use", (req, res) => {
-  connection.query(USER_QUERY, [USER_ID], (err, results) => {
+  connection.query(GET_USER_QUERY, [USER_ID], (err, results) => {
     if (err) throw err;
-    if (results[0].action_count === 0)
+    if (results[0].active_point === 0)
       return res.send(JSON.stringify({ code: 400 }));
-    const calculate = recoverAction(
-      results[0].action_count,
-      results[0].last_update_time
+    const now = Date.now();
+    const activePointCalculate = activePointMiddleWare(
+      results[0].active_point,
+      (results[0].last_update_ts > 0) ? results[0].last_update_ts : now,
     );
     connection.query(
-      "UPDATE user_action SET last_update_time = ?, action_count = ? WHERE user_id = ?",
-      [calculate.now, calculate.count - 1, USER_ID],
+      UPDATE_USER_QUERY,
+      [activePointCalculate.last_update_ts, activePointCalculate.active_point - 1, USER_ID],
       (err) => {
         if (err) throw err;
-        const now = Date.now();
         return res.send(
           JSON.stringify({
             code: 200,
-            action_count: calculate.count - 1,
-            last_update_time: calculate.now,
-            server_time: now,
+            active_point: activePointCalculate.active_point - 1,
+            last_update_ts: activePointCalculate.last_update_ts,
+            server_ts: now,
           })
         );
       }
@@ -88,17 +91,30 @@ app.get("/use", (req, res) => {
   });
 });
 
-function recoverAction(action_count, last_update_time) {
-  // 지난 사용 시간을 이용하여 충전해야 할 행동력 계산
+/**
+ * 서버에서 충전한 시간 또는 클라에서 사용한 시간을 이용하여 충전해줘야 할 행동력을 계산
+ * @param {Number} active_point 보유한 행동력
+ * @param {Number} last_update_ts 사용했었던 시간 또는 충전해줬었던 시간 (=단위는 ms)
+ */
+function activePointMiddleWare(active_point, last_update_ts) {
   const now = Date.now();
-  const lastUpdateTime = last_update_time > 0 ? last_update_time : now;
-  const chargeActionCount = Math.floor((now - lastUpdateTime) / CHARGE_TIME);
-  const addCount = action_count + chargeActionCount;
-  const totalCount = addCount >= 5 ? 5 : addCount;
-  let updateTime = lastUpdateTime;
-  if (chargeActionCount > 0) updateTime = now;
-  if (totalCount === 5) updateTime = 0;
-  return { count: totalCount, now: updateTime };
+  const results = {
+    active_point,
+    last_update_ts,
+  };
+  const chargeActionCount = Math.floor(
+    (now - results.last_update_ts) / CHARGE_TIME
+  ); // 충전 해줘야 하는 행동력 개수
+  if (chargeActionCount > 0) {
+    results.active_point += chargeActionCount;
+    results.last_update_ts += (chargeActionCount * CHARGE_TIME);
+  }
+  if (chargeActionCount > 0 && results.active_point >= MAX_ACTIVE_POINT) { // 충전했을 시 최대 개수가 넘으면
+    results.active_point = MAX_ACTIVE_POINT;
+    results.last_update_ts = 0;
+  }
+  // console.log("results", results);
+  return results;
 }
 
 app.listen(3000, () => console.log("서버 동작중"));
